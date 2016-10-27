2eecb90bcb9a13d769d734153dd8b88399beca3c
Mike Stubna <mike@stubna.com>
2016-10-27 07:07:42 -0400

    update

--- attributes/phantomjs.rb
+++ attributes/phantomjs.rb
@@ -1,2 +1,2 @@
-node['phantomjs']['base_url'] = 'https://bitbucket.org/ariya/phantomjs/downloads'
-node['phantomjs']['version'] = '2.1.1'
+normal['phantomjs']['base_url'] = 'https://bitbucket.org/ariya/phantomjs/downloads'
+normal['phantomjs']['version'] = '2.1.1'
